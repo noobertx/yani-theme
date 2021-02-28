c04f60ddce46b2bfeafcf1e315c5bb1a51783251
noobertx <noobertx@gmail.com>
2021-02-28 20:43:44 +0800

    Basic Files

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE <?php langiage_attributes();?>>
+<html>
+<head>
+	<meta charset="<?php bloginfo('charset');?>">
+	<meta name="viewport" content="width-device=width,initial-scale=1.0">
+	<meta http-equiv="X0UA-Compatible" content="ie=edge">
+	<?php wp_head();?>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php get_header();?>
+
+<?php if(have_posts()){ ?>
+	<?php while(have_posts()){ ?>
+		<?php the_post(){ ?>
+			<h2>
+				<a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>"> <?php the_title(); ?></a>
+			</h2>
+			<div>
+				Posted on 
+				<a href="<?php echo get_permalink()?>">
+					<time date-time="<?echo get_the_date("c");?>">
+						<?php echo get_the_date();?>
+					</time>
+				</a>
+				By 
+				<a href="<?echo get_author_posts_ur(get_the_author_meta('ID'));?>">
+					<?php echo get_the_author();?>
+				</a>
+			</div>
+			<div>
+				<?php the_excerpt();?>
+			</div>
+			<a href="<?php echo get_the_permalink()?>" title="<?php the_title_attribute()?>">
+				Read More
+				<span class="u-screen-reader-text">About <?php the_title()?></span>
+			</a>
+	<?php }?>
+	<?php the_post_pagination(); ?>
+<?php }?>
+
+<?php get_footer();?>
