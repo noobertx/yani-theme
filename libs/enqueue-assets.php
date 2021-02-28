330222cb7f7f8253252a058350d89e98cda77aa2
noobertx <noobertx@gmail.com>
2021-02-28 21:19:40 +0800

    Admin assets

--- libs/enqueue-assets.php
+++ libs/enqueue-assets.php
@@ -4,6 +4,14 @@ function yani_assets(){
 	wp_enqueue_style("yani-stylesheet",get_template_directory_uri()."/dist/assets/css/bundle.css",array(),microtime(),"all");
 }
 
+
 add_action("wp_enqueue_scripts","yani_assets");
+
+function yani_admin_assets(){
+	wp_enqueue_style("yani-stylesheet",get_template_directory_uri()."/dist/assets/css/admin.css",array(),microtime(),"all");
+}
+
+
+add_action("wp_admin_enqueue_scripts","yani_admin_assets");
 
 ?>
