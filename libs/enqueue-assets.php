6e380e6fdbad990e095f3ee917e46e408a4ca5dc
noobertx <noobertx@gmail.com>
2021-03-01 22:34:13 +0800

    Enqueue scripts

--- libs/enqueue-assets.php
+++ libs/enqueue-assets.php
@@ -2,16 +2,18 @@
 
 function yani_assets(){
 	wp_enqueue_style("yani-stylesheet",get_template_directory_uri()."/dist/assets/css/bundle.css",array(),microtime(),"all");
-}
+	wp_enqueue_script("yani-script",get_template_directory_uri()."/dist/assets/js/bundle.js",array(),microtime(),true);
+} 
 
 
 add_action("wp_enqueue_scripts","yani_assets");
 
 function yani_admin_assets(){
-	wp_enqueue_style("yani-stylesheet",get_template_directory_uri()."/dist/assets/css/admin.css",array(),microtime(),"all");
+	wp_enqueue_style("yani-admin-stylesheet",get_template_directory_uri()."/dist/assets/css/admin.css",array(),microtime(),"all");
+	wp_enqueue_script("yani-admin-script",get_template_directory_uri()."/dist/assets/js/admin.js",array(),microtime(),true);
 }
 
 
-add_action("wp_admin_enqueue_scripts","yani_admin_assets");
+add_action("admin_enqueue_scripts","yani_admin_assets");
 
 ?>
