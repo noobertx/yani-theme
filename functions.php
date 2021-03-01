4c00e91488a49fe1b984369ff11bd02dfe6ae1bc
noobertx <noobertx@gmail.com>
2021-03-02 03:13:26 +0800

    Added Font management

--- functions.php
+++ functions.php
@@ -2,4 +2,5 @@
 <?php require_once("libs/sidebars.php");?>
 <?php require_once("libs/rest-api.php");?>
 <?php require_once("libs/navigation.php");?>
+<?php require_once("libs/fonts.php");?>
 <?php require_once("libs/customizer.php");?>
---
+++ libs/fonts.php
@@ -0,0 +1,69 @@
+<?php
+class Theme_Fonts{
+	protected $google_fonts;
+
+	function __construct(){
+		add_action( 'wp_enqueue_scripts', [ $this, 'action_enqueue_styles' ] );
+	}
+
+	public function action_enqueue_styles(){
+		$google_fonts_url = $this->get_google_fonts_url();
+		if ( ! empty( $google_fonts_url ) ) {
+			wp_enqueue_style( 'yani-fonts', $google_fonts_url, [], null ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+		}
+	}
+
+	protected function get_google_fonts_url() : string {
+		$google_fonts = $this->get_google_fonts();
+
+		if ( empty( $google_fonts ) ) {
+			return '';
+		}
+
+		$font_families = [];
+
+		foreach ( $google_fonts as $font_name => $font_variants ) {
+			if ( ! empty( $font_variants ) ) {
+				if ( ! is_array( $font_variants ) ) {
+					$font_variants = explode( ',', str_replace( ' ', '', $font_variants ) );
+				}
+
+				$font_families[] = $font_name . ':' . implode( ',', $font_variants );
+				continue;
+			}
+
+			$font_families[] = $font_name;
+		}
+
+		$query_args = [
+			'family'  => implode( '|', $font_families ),
+			'display' => 'swap',
+		];
+
+		return add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	protected function get_google_fonts() : array {
+		if ( is_array( $this->google_fonts ) ) {
+			return $this->google_fonts;
+		}
+
+		$google_fonts = [
+			'Roboto' => [ '400', '400i', '700', '700i' ],
+			'Poppins'     => [ '400', '400i', '600', '600i' ],
+			'RocknRoll+One'     => [ '400', '400i', '600', '600i' ],
+		];
+
+		/**
+		 * Filters default Google Fonts.
+		 *
+		 * @param array $google_fonts Associative array of $font_name => $font_variants pairs.
+		 */
+		$this->google_fonts = (array) apply_filters( 'yani_google_fonts', $google_fonts );
+
+		return $this->google_fonts;
+	}
+}
+
+new Theme_Fonts();
+?>
