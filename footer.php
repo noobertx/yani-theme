ebd6b6a27ce715f97c3e5873e2743611b25b3b23
noobertx <noobertx@gmail.com>
2021-03-01 15:31:04 +0800

    Added Footer Widget Areas

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
+		<footer class="bg-dark">
+			<div id="footer-wrap" >
+				<?php for($i=0;$i<4;++$i){ ?>
+					<div class="footer-<?php echo $i;?>">						
+						<?php dynamic_sidebar( 'footer-sidebar-'.$i ); ?>
+					</div>
+				<?php } ?>
+			</div>
+		</footer>
 		<?php wp_footer();?>
 	</body>
 </html>
--- libs/sidebars.php
+++ libs/sidebars.php
@@ -12,4 +12,20 @@ function _themename_sidebar_widgets(){
 }
 
 add_action('widgets_init','_themename_sidebar_widgets');
+function _themename_footer_widgets(){
+	$columns = [3,3,3,3];
+
+	foreach($columns as $i => $column){
+		register_sidebar(array(
+			'id' => 'footer-sidebar-'.$i,
+			'name' => esc_html(__('Footer Sidebar ('.($i+1).')' ,'_theme_name')),
+			'description' => esc_html(__('Footer Widgets','_theme_name')),
+			'before_widget' => '<section id="%1$s" class="">',
+			'after_widget' => '</section>',
+			'before_title' => '<h5>',
+			'after_title' => '</h5>',
+		));
+	}
+}
+add_action('widgets_init','_themename_footer_widgets');
 ?>
