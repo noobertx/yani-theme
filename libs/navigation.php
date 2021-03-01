d241097f232ae087cf3fab81360161461450ee10
noobertx <noobertx@gmail.com>
2021-03-02 00:48:09 +0800

    Added dropdown arrow

--- libs/navigation.php
+++ libs/navigation.php
@@ -5,5 +5,20 @@ function _themename_register_menus(){
 	));
 }
 
-add_action('init','_themename_register_menus')
+add_action('init','_themename_register_menus');
+function _themename_dropdown_icon($title,$item,$args,$depth){
+
+	if($args->theme_location == 'main-menu'){
+		if(in_array("menu-item-has-children",$item->classes)){
+			if($depth == 0){
+				$title .= "<i class='fas fa-angle-down' aria-hidden='true'></i>";
+			}else{
+				$title .= "<i class='fa fa-angle-down' aria-hidden='true'></i>";
+			}
+		}
+	}
+	return $title;
+}
+
+add_filter('nav_menu_item_title','_themename_dropdown_icon',10,4);
 ?>
