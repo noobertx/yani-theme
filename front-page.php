769d4f279834caae037784c89fbebe8634fd141b
noobertx <noobertx@gmail.com>
2021-03-01 13:19:16 +0800

    Initial Frontpage

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+<div class="square bg-primary"></div>
+<div class="square bg-secondary"></div>
+<div class="square bg-acccent"></div>
+<div class="square bg-light"></div>
+<div class="square bg-dark"></div>
+
+<main>
+<?php the_title();?>
+<?php the_content();?>
+	
+</main>
+
+
+<?php get_sidebar();?>
+
+<?php get_footer();?>
