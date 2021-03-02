3930b6d4dbdd2c24b807362600af8ede440db9ff
noobertx <noobertx@gmail.com>
2021-03-03 05:42:35 +0800

    Initial single

---
+++ single.php
@@ -0,0 +1,14 @@
+<?php get_header();?>
+<?php $sidebarClass = (is_active_sidebar('primary-sidebar')) ? "has-sidebar" : "" ?>
+<div id="page" class="<?php echo $sidebarClass;?>">
+
+	
+<main>
+<?php get_template_part('loop'); ?>
+</main>
+<?php if(is_active_sidebar('primary-sidebar')) { ?>
+<?php get_sidebar();?>
+<?php } ?>
+</div>
+
+<?php get_footer();?>
