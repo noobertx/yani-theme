83b884d00b12be3a04ac1fc9aafcc2d9557395d4
noobertx <noobertx@gmail.com>
2021-03-03 20:04:40 +0800

    Added Theme supports

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php require_once("libs/enqueue-assets.php");?>
+<?php require_once("libs/theme-support.php");?>
 <?php require_once("libs/sidebars.php");?>
 <?php require_once("libs/rest-api.php");?>
 <?php require_once("libs/navigation.php");?>
