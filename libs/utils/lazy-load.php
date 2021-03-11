2d11c430ef44304e2855e530f30daf559e1a8141
noobertx <noobertx@gmail.com>
2021-03-11 16:36:04 +0800

    Resolve Utils path

--- libs/utils/lazy-load.php
+++ libs/utils/lazy-load.php
@@ -23,9 +23,9 @@ class Yani_LazyLoad {
 		}
 
 		// If the AMP plugin is active, return early.
-		if ( wp_rig()->is_amp() ) {
-			return;
-		}
+		// if ( wp_rig()->is_amp() ) {
+		// 	return;
+		// }
 
 		add_action( 'wp_head', [ $this, 'action_add_lazyload_filters' ], PHP_INT_MAX );
 		add_action( 'wp_enqueue_scripts', [ $this, 'action_enqueue_lazyload_assets' ] );
