f3b0929818eba9fba9cf4a32ffe53ac2474bb92e
noobertx <noobertx@gmail.com>
2021-03-23 04:39:21 +0800

    Renders dynamic Copyright

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -1,1 +1,10 @@
-<div class="info-wrap bg-dark light">All rights reserved</div>
+<?php 
+	global $custom_wprig_opt;
+?>
+<div class="info-wrap bg-dark light text-center">
+	<?php if ($custom_wprig_opt['opt-display-copyright'] && !empty($custom_wprig_opt['opt-display-copyright-text'])) {
+		echo $custom_wprig_opt['opt-display-copyright-text'];
+	}else{
+		echo "All rights reserved";
+	} ?>
+</div>
