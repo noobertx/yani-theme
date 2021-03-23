2d5cf903f2724544b735ca0e0c2a625bb58c3ad3
noobertx <noobertx@gmail.com>
2021-03-24 07:31:02 +0800

    Slider Background Revision

--- page-templates/page-woo-homepage.php
+++ page-templates/page-woo-homepage.php
@@ -10,11 +10,56 @@ Template Name:Woocommerce Homepage Page
 <div id="page" class="<?php echo $sidebarClass;?> full-width-page">
 
 <main >
+	<section class="main-slider">
 
-<?php $hide_title = get_post_meta(get_the_ID(),"__themename_hide_title",false)[0]; ?>
-	
+		<div class="slick page-slider">
+			<?php for($i =1; $i<4 ; ++$i){ 
+				$slider_page[$i] 		= get_theme_mod('set_slider_page'.$i);
+				$slider_button_text[$i] = get_theme_mod('set_slider_button_text'.$i);
+				$slider_button_url[$i] 	= get_theme_mod('set_slider_button_url'.$i);
+			?>
+			<?php } ?>
+
+			<?php
+				$j = 1;
+				$args = [
+					'post_type' => "page",
+					'post_per_page' => 3,
+					'post__in' => $slider_page,
+					'orderby' => "post__in",
+				];
+
+				$slider_loop = new WP_Query($args);
+
+				if($slider_loop->have_posts()){
+					while($slider_loop->have_posts()){
+						$slider_loop->the_post();
+						?>
+							<div style="background-image:url('<?php echo get_the_post_thumbnail_url(get_the_ID());?>');background-size:cover;">															
+								
+								<div class="container">
+									<div class="slider-details-container">
+										<div class="slide-title">
+											<h1><?php the_title();?></h1>
+										</div>
+										<div class="slide-description">
+											<div class="subtitle">
+												<?php //the_excerpt();?>
+											</div>
+											<a href="<?php echo $slider_button_url[$j] ?>" class="link"><?php echo $slider_button_text[$j] ?></a>
+										</div>
+									</div>
+								</div>
+							</div>
+						<?php
+						++$j ;
+					}
+				}
+			?>
+		</div>
+	</section>	
 	<div class="page__content">
-		<?php the_content();?>			
+		<?php //the_content();?>			
 	</div>
 </main>
 
