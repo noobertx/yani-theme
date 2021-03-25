46a0d54ed1d844adfe01291bbc663355bd678d75
noobertx <noobertx@gmail.com>
2021-03-25 13:01:32 +0800

    MEtadescription on blogs

--- template-parts/post/header.php
+++ template-parts/post/header.php
@@ -3,6 +3,13 @@
 	<h2>
 		<a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"> <?php the_title(); ?></a>
 	</h2>
+	<div class="post-thumbnail">
+		<?php
+			if(has_post_thumbnail()): 
+				the_post_thumbnail('medium',['class'=>'img-fluid']);
+			endif;
+		?>
+	</div>	
 	<div>
 		Posted on 
 		<a href="<?php echo get_permalink()?>">
@@ -14,5 +21,12 @@
 		<a href="<?echo get_author_posts_url(get_the_author_meta('ID'));?>">
 			<?php echo get_the_author();?>
 		</a>
+
+		<?php if(has_category()): ?>
+		Categories <span><?php the_category('');?></span>
+		<?php endif;?>
+		<?php if(has_tags()): ?>
+		Tags <span><?php the_tags('',',');?></span>
+		<?php endif;?>
 	</div>
 	</div>
