3720cd531dc41412649997a2a56ee80d94cfed86
noobertx <noobertx@gmail.com>
2021-03-26 22:45:07 +0800

    Sidebar position frontpage

--- front-page.php
+++ front-page.php
@@ -1,6 +1,17 @@
 <?php get_header();?>
-<?php $sidebarClass = (is_active_sidebar('primary-sidebar')) ? "has-sidebar" : "" ?>
+<?php $sidebarClass = (is_active_sidebar('primary-sidebar')) ? ($custom_wprig_opt['opt-page-layout']=="left") ? "has-sidebar sidebar-left":"has-sidebar sidebar-right" : "" ?>
+<?php
+	global $custom_wprig_opt;
+?>
+
 <div id="page" class="<?php echo $sidebarClass;?>">
+	<?php
+		if($custom_wprig_opt['opt-page-layout']=="left-sidebar"):
+			if(is_active_sidebar('primary-sidebar')) : 
+				get_sidebar();
+			endif;
+		endif;
+	?>
 
 <main >
 
@@ -9,9 +20,13 @@
 
 </main>
 
-<?php if(is_active_sidebar('primary-sidebar')) { ?>
-	<?php get_sidebar();?>
-<?php } ?>
+	<?php
+		if($custom_wprig_opt['opt-page-layout']=="right-sidebar"):
+			if(is_active_sidebar('primary-sidebar')) : 
+				get_sidebar();
+			endif;
+		endif;
+	?>
 </div>
 
 <?php get_footer();?>
