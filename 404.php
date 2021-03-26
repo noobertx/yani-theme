8a863f2ce2663f7062b620082abb43c0c290a14b
noobertx <noobertx@gmail.com>
2021-03-27 02:54:42 +0800

    Resolve 404.php issue

--- 404.php
+++ 404.php
@@ -2,13 +2,7 @@
 <?php $sidebarClass = (is_active_sidebar('primary-sidebar')) ? "has-sidebar" : "" ?>
 <div id="page" class="<?php echo $sidebarClass;?>">
 <main>
-	<header>
-		<h3>
-			<?php _e("Nothing Found here, Try to search ?","_themename"); ?>
-		</h3>
-
-		<?php get_search_form(); ?>
-	</header>
+	
 	<?php get_template_part('loop'); ?>
 </main>
 <?php if(is_active_sidebar('primary-sidebar')) { ?>
