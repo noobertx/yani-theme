a4fd9a40d8e021587953937bfbd4b2de0f0e1285
noobertx <noobertx@gmail.com>
2021-03-26 21:08:52 +0800

    Pagination styles

--- loop.php
+++ loop.php
@@ -3,8 +3,9 @@
 		<?php the_post(); ?>
 		<?php get_template_part('template-parts/post/content',get_post_format()); ?>
 	<?php }?>
-
+<section>	
 	<?php the_posts_pagination(); ?>
+</section>
 <?php }else{ ?>
 		<?php get_template_part('template-parts/post/content','none'); ?>
 <?php } ?>
