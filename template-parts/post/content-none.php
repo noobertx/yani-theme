560d4c80f6507a0faf66d0880dc051ee8fbae63f
noobertx <noobertx@gmail.com>
2021-03-26 22:29:47 +0800

    Recent post if no results found feature

--- template-parts/post/content-none.php
+++ template-parts/post/content-none.php
@@ -1,4 +1,9 @@
 <div class="search-result-none text-center">	
 <h3><?php esc_html_e("Nothing Found here, Try to search ?","_themename"); ?></h3>
 <?php get_search_form(); ?>
+
+<?php the_widget('WP_Widget_Recent_Posts',[
+	'title' => 'Take a look at out Lastest Post',
+	'number' => 3
+]); ?>
 </div>
