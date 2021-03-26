8a3ed1b0446a4b07a991f3794e53ad0e95782a26
noobertx <noobertx@gmail.com>
2021-03-27 00:11:21 +0800

    Removed Sidebar on frontpage

--- front-page.php
+++ front-page.php
@@ -1,18 +1,8 @@
 <?php get_header();?>
-<?php $sidebarClass = (is_active_sidebar('primary-sidebar')) ? ($custom_wprig_opt['opt-page-layout']=="left") ? "has-sidebar sidebar-left":"has-sidebar sidebar-right" : "" ?>
-<?php
-	global $custom_wprig_opt;
-?>
 
-<div id="page" class="<?php echo $sidebarClass;?>">
-	<?php
-		if($custom_wprig_opt['opt-page-layout']=="left-sidebar"):
-			if(is_active_sidebar('primary-sidebar')) : 
-				get_sidebar();
-			endif;
-		endif;
-	?>
 
+<div id="page">
+	
 <main >
 
 
@@ -20,13 +10,6 @@
 
 </main>
 
-	<?php
-		if($custom_wprig_opt['opt-page-layout']=="right-sidebar"):
-			if(is_active_sidebar('primary-sidebar')) : 
-				get_sidebar();
-			endif;
-		endif;
-	?>
 </div>
 
 <?php get_footer();?>
