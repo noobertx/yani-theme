61d3476b73e6cb8454a3136f49d642a18a4697a6
noobertx <noobertx@gmail.com>
2022-02-04 03:54:23 +0800

    Initial Custom loop query

--- functions.php
+++ functions.php
@@ -140,3 +140,41 @@ function filter_single_post_pagination($output, $format, $link, $post){
 }
 add_filter( 'previous_post_link', 'filter_single_post_pagination', 10, 4);
 add_filter( 'next_post_link', 'filter_single_post_pagination', 10, 4);
+
+
+/*
+add_action('pre_get_posts', 'university_adjust_queries');
+function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('campus') AND is_main_query()) {
+    $query->set('posts_per_page', -1);
+  }
+
+  if (!is_admin() AND is_post_type_archive('program') AND is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
+  }
+  if (!is_admin() AND is_post_type_archive('packages_post') AND is_main_query()) {
+    $query->set('orderby', 'post_ID');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
+  }
+  if (!is_admin() AND is_post_type_archive('employer') AND is_main_query()) {
+    $query->set('posts_per_page', -1);
+  }
+  if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            ));
+  }
+}
+*/
