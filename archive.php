0c7d8b4b7fdbf4bfa1100770a45d13243b62d342
noobertx <noobertx@gmail.com>
2022-02-04 20:52:36 +0800

    Archive revision

--- archive.php
+++ archive.php
@@ -3,7 +3,7 @@
 <div id="page" class="<?php echo $sidebarClass;?>">
 <main>	
 	<section class="page-banner">		
-		<header>
+		<header class="container mx-auto">
 			<?php _e(the_archive_title('<h1 class="light article-title">','</h1>'),'_themename');?>
 			<?php _e(the_archive_description('<p>','</p>'),'_themename');?>
 		</header>
