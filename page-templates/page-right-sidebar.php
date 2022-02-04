262a52a4b63eed7e3d4b20d97325d50a804fb11c
noobertx <noobertx@gmail.com>
2022-02-04 20:51:12 +0800

    page revisions

--- page-templates/page-right-sidebar.php
+++ page-templates/page-right-sidebar.php
@@ -13,14 +13,14 @@ Template Name:Right Sidebar Page
 
 <?php $hide_title = get_post_meta(get_the_ID(),"__themename_hide_title",false)[0]; ?>
 	<header class="page__header">
-		<div class="page__title">
+		<div class="page__title container mx-auto">
 			<?php 
 				if($hide_title!="yes" ){
 					the_title();
 				} ?>	
 		</div>
 	</header>
-	<div class="page__content">
+	<div class="page__content container mx-auto">
 		<?php the_content();?>			
 	</div>
 </main>
--- page.php
+++ page.php
@@ -1,8 +1,11 @@
 <?php get_header();?>
-<?php $sidebarClass = (is_active_sidebar('primary-sidebar')) ? "has-sidebar" : "" ;?>
-<?php $sidebarClass ="";?>
-<div id="page" class="<?php echo $sidebarClass;?>">
+<?php $sidebarClass = (is_active_sidebar('primary-sidebar')) ? "container mx-auto" : "" ;?>
+<?php $sidebarClass = "";
+?>
+<div id="page  " class="d-flex justify-space-between <?php echo $sidebarClass;?>">
 <?php
+	// $custom_wprig_opt['opt-page-layout']="left-sidebar";
+	$custom_wprig_opt['opt-page-layout']="right-sidebar";
 		if(isset($custom_wprig_opt) && $custom_wprig_opt['opt-page-layout']=="left-sidebar"):
 			if(is_active_sidebar('primary-sidebar')) : 
 				get_sidebar();
@@ -10,29 +13,30 @@
 		endif;
 	?>
 
-<main >
+<main class="<?php echo ($sidebarClass!="") ? "main-container": ""; ?>">
 
 <?php //$hide_title = get_post_meta(get_the_ID(),"__themename_hide_title",false)[0]; ?>
 <?php $hide_title = "no"; ?>
-	<header class="page__header">
-		<div class="page__title asdasd">
+	<header class="page__header container mx-auto">
+		<div class="page__title">
 			<?php 
 				if($hide_title!="yes" ){ ?>
 					<h1><?php the_title(); ?> </h1>
 				<?php  } ?>	
 		</div>
 	</header>
-	<div class="page__content">
+	<div class="page__content container mx-auto">
 		<?php the_content();?>			
 	</div>
 </main>
 
+<?php $sidebarClass = (is_active_sidebar('primary-sidebar')) ? "container mx-auto" : "" ;?>
 <?php
-		if(isset($custom_wprig_opt) && $custom_wprig_opt['opt-page-layout']=="right-sidebar"):
+		// if(isset($custom_wprig_opt) && $custom_wprig_opt['opt-page-layout']=="right-sidebar"):
 			if(is_active_sidebar('primary-sidebar')) : 
 				get_sidebar();
 			endif;
-		endif;
+		// endif;
 	?>
 
 </div>
