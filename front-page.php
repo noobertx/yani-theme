fad27c6a0a5fbb27a8328bac7aa6bd1b001302d0
noobertx <noobertx@gmail.com>
2022-02-04 20:51:23 +0800

    Hero Area

--- front-page.php
+++ front-page.php
@@ -1,15 +1,15 @@
 <?php get_header();?>
-
-
-<div id="page">
-	
-<main >
-
-
-<?php the_content();?>	
-
-</main>
-
-</div>
-
+	<div id="page">		
+		<main>
+			<div class="main-hero bg-dark text-light">
+				<div class="hero-content-wrap d-flex align-items-center">
+					<div class="hero-text-area text-center w-75 p-3 mx-auto">
+						<h1 class="hero-title">WELCOME TO THE SITE</h1>
+						<p class="hero-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae velit praesentium quia deserunt pariatur porro, tenetur explicabo fugiat, enim dolor provident magni veniam maxime impedit eaque? Provident maxime reprehenderit cum!</p>
+						<a href="#" class="btn btn-primary hero-cta">Get Started</a>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
 <?php get_footer();?>
